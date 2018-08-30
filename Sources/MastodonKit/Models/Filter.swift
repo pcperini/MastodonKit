494fdb3284c37752d55e5057984f51ce44b862bc
Patrick Perini <pcperini@gmail.com>
2018-08-30 10:00:15 -0700

    finalizing

--- Sources/MastodonKit/Models/Filter.swift
+++ Sources/MastodonKit/Models/Filter.swift
@@ -12,20 +12,20 @@ public class Filter: Codable {
     /// String that contains keyword or phrase.
     public let phrase: String
     /// Array of strings that means filtering context. Each string is one of 'home', 'notifications', 'public', 'thread'. At least one context must be specified.
-    public let contexts: String
+    public let contexts: [String]
     /// Boolean that indicates irreversible filtering on server side.
     public let irreversible: Bool?
     /// Boolean that indicates word match.
     public let wholeWord: Bool?
     /// Number that indicates seconds. Filter will be expire in seconds after API processed. Null or blank string means "don't change". Default is unlimited.
-    public let expiresIn: Int?
+    public let expiresAt: String?
     
     private enum CodingKeys: String, CodingKey {
         case phrase
-        case context
+        case contexts = "context"
         case irreversible
         case wholeWord = "whole_word"
-        case expiresIn = "expires_in"
+        case expiresAt = "expires_at"
     }
     
     @available(*, deprecated, message: "Do not use.")
